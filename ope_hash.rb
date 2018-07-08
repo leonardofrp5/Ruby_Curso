ea8b1f81bec1cbe549daad572f970e0d3ab3abb4
leonardo <leonardofrp@hotmail.com>
2018-07-08 17:14:03 -0500

    practica con operadores de array o metodos

---
+++ ope_hash.rb
@@ -0,0 +1,11 @@
+datos = {:name => "Andrea", :last_name => "Galvis Munoz", :age => 22}
+datos_two = {:name_ => "Leonardo", :last_name_ => "Romero Perez", :age_ => 25}
+
+#datos.clear
+
+#puts datos.size
+#puts datos.keys
+#puts datos.values
+#puts datos.empty?
+#puts datos.invert
+puts datos.merge(datos_two)
